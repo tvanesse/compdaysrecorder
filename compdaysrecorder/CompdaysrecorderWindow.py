6b16b1856a9e0af3db2e5d0b7eb331e0a0a18baf
Thomas Vanesse <vanessethomas@gmail.com>
2014-03-16 01:56:10 +0100

    Renamed on_calendar_daySelected() to on_calendar_day_selected().

--- compdaysrecorder/CompdaysrecorderWindow.py
+++ compdaysrecorder/CompdaysrecorderWindow.py
@@ -1,11 +1,22 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# This file is in the public domain
+# Copyright (C) 2014 Thomas Vanesse <vanessethomas@gmail.com>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
-import gettext
-from gettext import gettext as _
-gettext.textdomain('compdaysrecorder')
+import locale
+from locale import gettext as _
+locale.textdomain('compdaysrecorder')
 
 from gi.repository import Gtk, Gdk # pylint: disable=E0611
 import logging
@@ -131,7 +142,7 @@ class CompdaysrecorderWindow(Window):
         self.compDaysLabel.set_text(str(off/8))
         
 
-    def on_calendar_daySelected(self, widget):
+    def on_calendar_day_selected(self, widget):
         # Parse the date selected
         year, month, day = self.calendar.get_date()
         dateObj = date(year, month+1, day)
--- compdaysrecorder/__init__.py
+++ compdaysrecorder/__init__.py
@@ -1,13 +1,24 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# This file is in the public domain
+# Copyright (C) 2014 Thomas Vanesse <vanessethomas@gmail.com>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 import optparse
 
-import gettext
-from gettext import gettext as _
-gettext.textdomain('compdaysrecorder')
+import locale
+from locale import gettext as _
+locale.textdomain('compdaysrecorder')
 
 from gi.repository import Gtk # pylint: disable=E0611
 
